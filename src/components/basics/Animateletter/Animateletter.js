be5186189b5d3b78ec7ad8175648922ddca8c2c5
Dilpreet0501 <100590445+Dilpreet0501@users.noreply.github.com>
2023-10-05 03:17:54 +0530

    responsiveness done

--- src/components/basics/Animateletter/Animateletter.js
+++ src/components/basics/Animateletter/Animateletter.js
@@ -1,38 +1,16 @@
 import React from 'react'
 import "./Animateletter.scss"
-import { useEffect, useState } from 'react';
+
 
 
 const Animateletter = ({strArray,letterClass,idx }) => {
   console.log(strArray);
-//   const [scrollPosition, setScrollPosition] = useState(0);
-// const handleScroll = () => {
-//     const position = window.pageYOffset;
-//     setScrollPosition(position);
 
-// };
-
-// useEffect(() => {
-//     window.addEventListener('scroll', handleScroll, { passive: true });
-
-//     return () => {
-//         window.removeEventListener('scroll', handleScroll);
-//     };
-// }, []);
-
-
-
-  
-//   var _window = window,Splitting = _window.Splitting,ScrollOut = _window.ScrollOut;
-// Splitting();
-// ScrollOut({
-//   targets: '.word',
-//   scrollingElement: '.page' });
 
   return (
    <>
    
-  {/* <div  className={`animated-text ${isVisible ? 'visible' : ''}`}> */}
+
       { strArray && strArray.map((item,i)=>(
   <span  key={i+item }  className={`${letterClass} _${i+idx}`}  >{item} </span>
     
